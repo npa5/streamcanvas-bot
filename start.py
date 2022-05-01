4d1dd622b951608c75926965b1ef6983b99d62ce
npa5 <97085060+npa5@users.noreply.github.com>
2022-05-01 18:22:38 +0200

    Update start.py

--- start.py
+++ start.py
@@ -14,9 +14,8 @@ if __name__ == "__main__":
     accounts = conf["accounts"]["accounts"]
     placet = {}
     completeIn = int(size[0]*size[1]*conf["settings"]["delay"]/len(conf["accounts"]["accounts"]))
-    
-    print(completeIn)
     log.info(f"Starting threads all threads, job will be done in about {datetime.timedelta(seconds =completeIn)} hours.")
+    
     for acc in accounts:
         placet[acc] = threading.Thread(target=place.loop, args=(acc,img,size,start))
         placet[acc].start()
