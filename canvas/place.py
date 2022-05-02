b85f5d62c57a515467db753334d80a4bb5520092
npa5 <97085060+npa5@users.noreply.github.com>
2022-05-02 15:37:38 +0200

    Update place.py

--- canvas/place.py
+++ canvas/place.py
@@ -1,4 +1,5 @@
 import canvas.log as log
+import canvas.account as account
 import threading
 import time
 import toml
@@ -17,27 +18,30 @@ def place(color, pixel, token, sio):
     sio.emit("p", {"t": token, "x": pixel[0], "y": pixel[1], "c": color})
     log.info(f"Placed pixel {color} at {pixel}")
 
-def loop(acc, img, size, start):
+def loop(acc, img, size, start, channel):
     global sio
     global i
     global n
     global pi
+    acctoken = account.auth(acc, channel)
     conf = toml.load("config.toml")
     sio[acc] = socketio.Client()
     sio[acc].connect(f'https://api.streamcanvas.raven.fo/{conf["settings"]["channel"]}')
-    log.info(f"started thread {acc}")
+    log.info(f"started thread {acctoken}")
 
     while True:
-    
         if img[i] != "000000":
             time.sleep(conf["settings"]["delay"])
-            place(img[i], [start[0] + pi, start[1] + n], acc, sio[acc])
+            place(img[i], [start[0] + pi, start[1] + n], acctoken, sio[acc])
+
+        if n == size[1]:
+            exit()
 
         if not pi == size[0]:
             i = i + 1
             pi = pi + 1
         else:
-            if n == size[1]:
-                exit()
+            acctoken = account.auth(acc, channel) # this should prevent all tokens from expiring
             pi = 0
             n = n + 1
+
