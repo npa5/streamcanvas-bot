fe3cfea8ed28824bb9c80003467d10edca5cf89c
npa5 <97085060+npa5@users.noreply.github.com>
2022-05-05 15:20:50 +0200

    changed some smaller stuff
    in preparation for switching to 2d arrays (ik i shouldve used those in the first place, shut up), also fixed bug with void mode requiring an image to run although it doesnt use said image

--- start.py
+++ start.py
@@ -10,15 +10,15 @@ import time
 if __name__ == "__main__":
     conf = config.load()
     settings = conf["settings"]
-    img = image.load(settings["image"])
-    size = image.size(settings["image"])
+    if settings["mode"] != "void":
+        img = image.load(settings["image"])
+    else:
+        img = None
     accounts = conf["accounts"]["accounts"]
     placet = {}
     token = {}
-    completeIn = int(size[0]*size[1]*conf["settings"]["delay"]/len(conf["accounts"]["accounts"]))
-
-    log.info(f"Starting threads all threads, job will be done in about {datetime.timedelta(seconds =completeIn)} hours.")
+    
     for i in range(len(accounts)):
-        placet[i] = threading.Thread(target=place.loop, args=(accounts[i],img,size,settings))
+        placet[i] = threading.Thread(target=place.loop, args=(accounts[i],img,settings))
         placet[i].start()
         time.sleep(conf["settings"]["offset"])
