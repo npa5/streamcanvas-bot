ed2cc6f56c55c2fa00ed5e9d9246874e20863173
npa5 <97085060+npa5@users.noreply.github.com>
2022-05-05 15:36:11 +0200

    Update config.py

--- canvas/config.py
+++ canvas/config.py
@@ -6,7 +6,7 @@ def load():
     try:
         config = toml.load("config.toml") # Attempts to load config file, creates new if not found
     except Exception: # BatChest USE "EXCEPT EXEPTION:"!!!!!
-        log.warn("Config file not found; creating config.")
+        log.warn("Config file not found/syntax error; creating new config.")
         with open("config.toml", "w") as f:
             f.write(requests.get("https://pastebin.com/raw/Zv9hryQj").text) # Gets config file from pastebin and writes it to config file
         log.info("Got default configuration and wrote it to config.toml.")
